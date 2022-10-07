508d0a357deb7196af5a62a5bc1964aa1548f713
Sam Xu <saxu@microsoft.com>
2022-10-07 11:39:31 -0700

    Make http://localhost:5168/odata/schools?$filter=ContactEmails/any(a: a eq 'help@mercury.com') works

--- OData/OData.WebApi/Extensions/SchoolStudentFilterBinder.cs
+++ OData/OData.WebApi/Extensions/SchoolStudentFilterBinder.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.OData.Query.Expressions;
 using Microsoft.OData.UriParser;
 using OData.WebApi.Models;
+using System.Data;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -8,10 +9,48 @@ namespace OData.WebApi.Extensions;
 
 public class SchoolStudentFilterBinder : FilterBinder
 {
+    public override Expression BindAnyNode(AnyNode anyNode, QueryBinderContext context)
+    {
+        // ?$filter=ContactEmails/any(a: a eq 'help@mercury.com')
+        if (anyNode.Source is CollectionPropertyAccessNode collectionPropertyAccessNode &&
+            string.Equals(collectionPropertyAccessNode.Property.Name, "ContactEmails", StringComparison.OrdinalIgnoreCase))
+        {
+            Expression source = context.CurrentParameter;
+
+            // $it.Emails
+            PropertyInfo emailsProperty = context.ElementClrType.GetProperty("Emails");
+            Expression propertyValue = Expression.Property(source, emailsProperty);
+
+            Expression constExp = null;
+            // It could be anything in the body, here, i just process the binary operator
+            BinaryOperatorNode bodyOperator = anyNode.Body as BinaryOperatorNode;
+            if (bodyOperator != null)
+            {
+                // Here, i just process the 'Eq'
+                if (bodyOperator.OperatorKind == BinaryOperatorKind.Equal &&
+                    bodyOperator.Right is ConstantNode constNode)
+                {
+                    constExp = Expression.Constant(constNode.Value);
+                }
+            }
+
+            if (constExp != null)
+            {
+                // string.Contains
+                MethodInfo containsMethodInfo = GetContainsMethodInfo();
+
+                // $it.Emails.Contains("....")
+                return Expression.Call(propertyValue, containsMethodInfo, constExp);
+            }
+        }
+
+        return base.BindAnyNode(anyNode, context);
+    }
+
     // change in operator
     public override Expression BindInNode(InNode inNode, QueryBinderContext context)
     {
-        // $filter='xyz' in Collection
+        // $filter='xyz' in ContactEmails
 
         if (inNode.Right is CollectionPropertyAccessNode rightCollectionPropertyAccessNode)
         {
